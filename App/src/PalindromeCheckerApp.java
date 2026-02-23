365ec76a9e73253010e8de6176d3b061409c0369
pd2341 <pd2341@srmist.edu.in>
2026-02-23 11:17:36 +0530

    UC3-HardCodePalidrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,14 +1,14 @@
- class PalindromeCheck {
+public class PalindromeCheck {
     public static void main(String[] args) {
-        String str = "madam"; // Hardcoded string
+        String str = "level";   // Hardcoded string
         String reversed = "";
 
         // Reverse the string
         for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+            reversed = reversed + str.charAt(i);
         }
 
-        // Check if palindrome
+        // Check if the original and reversed strings are equal
         if (str.equals(reversed)) {
             System.out.println(str + " is a palindrome.");
         } else {
